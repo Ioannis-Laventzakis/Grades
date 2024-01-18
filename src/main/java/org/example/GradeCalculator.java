b21e05cf87a27bfac372b4d2284caec3d2506557
Ioannis84 <glaventzakis@gmail.com>
2024-01-18 17:23:24 +0100

    new files

--- src/main/java/org/example/GradeCalculator.java
+++ src/main/java/org/example/GradeCalculator.java
@@ -7,7 +7,16 @@ public class GradeCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        // Ask the user for number of subjects
         System.out.println("Enter the number of subject: ");
+        int numberOfSubjects = scanner.nextInt();
+
+        //Get scores for each subject
+        int totalScore = 0;
+        for (int i = 0; i < numberOfSubjects; i++) {
+            System.out.println("Enter the score for subject " + (i + 1) + ": ");
+            int score = scanner.nextInt();
+            totalScore += score;
 
         }
 }
