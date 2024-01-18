139510d2511fe23ee0df2013e017e1ada54393c8
Ioannis84 <glaventzakis@gmail.com>
2024-01-18 17:42:50 +0100

    new files

--- src/main/java/org/example/GradeCalculator.java
+++ src/main/java/org/example/GradeCalculator.java
@@ -3,27 +3,31 @@ package org.example;
 
 import java.util.Scanner;
 
+
 public class GradeCalculator {
     public static void main(String[] args) {
+        // Creating a Scanner object to take input from the user
         Scanner scanner = new Scanner(System.in);
 
-        // Ask the user for number of subjects
-        System.out.println("Enter the number of subject: ");
+        // Ask the user for the number of subjects
+        System.out.println("Enter the number of subjects: ");
         int numberOfSubjects = scanner.nextInt();
 
-        //Get scores for each subject
+        // Get scores for each subject
         int totalScore = 0;
         for (int i = 0; i < numberOfSubjects; i++) {
+            // Ask the user for the score of each subject
             System.out.println("Enter the score for subject " + (i + 1) + ": ");
             int score = scanner.nextInt();
-            totalScore += score;
+            totalScore += score; // Add the score to the total
 
         }
-        //Calculate the average score
-        double averageScore = totalScore / numberOfSubjects;
+
+        // Calculate the average score
+        double averageScore = (double) totalScore / numberOfSubjects;
         System.out.println("The average score is: " + averageScore);
 
-        //Determining letter grade
+        // Determine the letter grade based on the average score
         char letterGrade = ' ';
         if (averageScore >= 90) {
             letterGrade = 'A';
@@ -37,5 +41,5 @@ public class GradeCalculator {
             letterGrade = 'F';
         }
         System.out.println("The letter grade is: " + letterGrade);
-
-}}
+    }
+}
