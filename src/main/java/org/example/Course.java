214b435abe48df26e695b76a4b71eb0e85d00175
Ioannis84 <glaventzakis@gmail.com>
2024-01-20 12:53:44 +0100

    Readme updated

--- src/main/java/org/example/Course.java
+++ src/main/java/org/example/Course.java
@@ -1,4 +1,32 @@
 package org.example;
 
 public class Course {
+
+    int courseID;
+    String courseName;
+    int numberOfStudents;
+
+    public int getCourseID() {
+        return courseID;
+    }
+
+    public void setCourseID(int courseID) {
+        this.courseID = courseID;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public int getNumberOfStudents() {
+        return numberOfStudents;
+    }
+
+    public void setNumberOfStudents(int numberOfStudents) {
+        this.numberOfStudents = numberOfStudents;
+    }
 }
